300c16f33b30995ad0672689fcc4bb559f39ec71
Dakota States <dakota.states@gmail.com>
2020-02-19 06:32:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -14,5 +14,19 @@ class Book
   def author
     @author
   end
+  def page_count=(num)
+    @num = num
+  end
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
 end
 
